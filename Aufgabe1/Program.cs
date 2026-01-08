1f6d5ea3366daad5e6f745c9bee51cdb645d0975
CodeAM-a11y <mutaf@mailbox.org>
2026-01-08 13:50:45 +0100

    first

---
+++ Aufgabe1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+
+namespace Aufgabe1;
+
+class Person
+{
+    public string vorname { get; set; }
+    public string name { get; set; }
+    public int alter { get; set; }
+
+    public Person(string vorname,string name, int alter)
+    {
+        this.vorname = vorname;
+        this.name = name;
+        this.alter = alter;
+    }
+
+    public override string ToString()
+    {
+        return $"Vorname: {vorname}, Name: {name}, Alter: {alter}";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        string BenutzerEingabe;
+        Console.WriteLine("Möchten Sie Person eingeben oder Laden:");
+        Console.WriteLine("Laden");
+        Console.WriteLine("Speichern");
+        BenutzerEingabe = Console.ReadLine();
+        if (BenutzerEingabe == "Speichern")
+        {
+            Console.WriteLine("Vornamen eingeben");
+            string tempVorname=Console.ReadLine();
+            Console.WriteLine("Nachnamen eingeben");
+            string tempName=Console.ReadLine();
+            Console.WriteLine("Alter eingeben");
+            int tempAlter=Convert.ToInt32(Console.ReadLine());
+            Person tempPerson = new Person(tempVorname, tempName, tempAlter);
+            string JsonString= JsonSerializer.Serialize(tempPerson,new JsonSerializerOptions(){WriteIndented =  true});
+            File.WriteAllText($"{tempPerson.name}.json", JsonString);
+            Console.WriteLine("Person erfolgreich gespeichert");
+        }
+        else if (BenutzerEingabe == "Laden")
+        {
+            Console.WriteLine("Personen Name eingeben");
+            string PersonenName = Console.ReadLine();
+            Person PersonLaden= 
+            JsonSerializer.Deserialize<Person>(File.ReadAllText($"{PersonenName}.json"));
+            Console.WriteLine("Geladene Person:");
+            Console.WriteLine(PersonLaden);
+        }
+    }
+}
---
+++ Aufgabe2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+using System.Text.Json;
+
+namespace Aufgabe1;
+
+class Person
+{
+    public string vorname { get; set; }
+    public string name { get; set; }
+    public int alter { get; set; }
+    public DateTime geburtsdatum { get; set; }
+
+    public Person(string vorname,string name, int alter, DateTime geburtsdatum)
+    {
+        this.vorname = vorname;
+        this.name = name;
+        this.alter = alter;
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public override string ToString()
+    {
+        return $"Vorname: {vorname}, Name: {name}, Alter: {alter}, Geburstdatum: {geburtsdatum}";
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> ListePersonen = new List<Person>();
+        string BenutzerEingabe;
+        Boolean ProgrammFortführen = true;
+        while (ProgrammFortführen)
+        {
+            Console.WriteLine("Möchten Sie Person eingeben oder Laden:");
+            Console.WriteLine("Laden");
+            Console.WriteLine("Speichern");
+            Console.WriteLine("Löschen");
+            Console.WriteLine("Beenden");
+            Console.WriteLine("Liste");
+            BenutzerEingabe = Console.ReadLine();
+            if (BenutzerEingabe == "Speichern")
+            {
+                Console.WriteLine("Wie viele Personen möchten Sie speichern?");
+                int AnzahlPersonen = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < AnzahlPersonen; i++)
+                {
+                    Console.WriteLine("Vornamen eingeben");
+                    string tempVorname = Console.ReadLine();
+                    Console.WriteLine("Nachnamen eingeben");
+                    string tempName = Console.ReadLine();
+                    Console.WriteLine("Alter eingeben");
+                    int tempAlter = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Geburstdatum eingeben");
+                    DateTime tempGeburstdatum = Convert.ToDateTime(Console.ReadLine());
+                    ListePersonen.Add(new Person(tempVorname, tempName, tempAlter, tempGeburstdatum));
+                }
+
+                JsonSerializerOptions options = new JsonSerializerOptions();
+                options.WriteIndented = true;
+                string JsonString = JsonSerializer.Serialize(ListePersonen, options);
+                File.WriteAllText("personList.json", JsonString);
+                Console.WriteLine("Personen erfolgreich gespeichert");
+            }
+            else if (BenutzerEingabe == "Laden")
+            {
+                ListePersonen =
+                    JsonSerializer.Deserialize<List<Person>>(File.ReadAllText("personList.json"));
+                Console.WriteLine("Geladene Person:");
+                foreach (Person item in ListePersonen)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else if (BenutzerEingabe == "Löschen")
+            {
+                ListePersonen.Clear();
+                Console.WriteLine("Erfolgreich Liste geleert");
+            }
+            else if (BenutzerEingabe=="Beenden")
+            {
+                ProgrammFortführen = false;
+            }
+            else if (BenutzerEingabe == "Liste")
+            {
+                foreach (Person item in ListePersonen)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
